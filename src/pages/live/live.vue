2e10a77b3b870b2ceb2e020063a62aa692506b49
xiaoandi666 <1437423842@qq.com>
2025-04-21 23:01:18 +0800

    完善tabbar

---
+++ src/pages/live/live.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="live">live</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ src/pages/refresh/refresh.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="refresh">refresh</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
